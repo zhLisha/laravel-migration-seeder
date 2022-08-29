7df5b8425e3ce0e4e62bae0bb5c36eedc2e0e539
zhLisha <lisazhou1709@gmail.com>
2022-08-29 14:16:21 +0200

    first commit

---
+++ database/migrations/2022_08_29_121430_create_travels_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+
+             // Destinazione
+             $table->string('destination', 30);
+             // Hotel
+             $table->string('hotel', 50);
+             // Servizi inclusi
+             $table->text('services');
+             // Partenza
+             $table->date('departure_day');
+             // Rientro
+             $table->date('return_day');
+             // Costo totale
+             $table->decimal('price', 8, 2)->unsigned();
+             
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
